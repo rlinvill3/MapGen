feab742a31fcb900a9ada176a0c052158f9ce59b
rlinvill3 <rlinvill@gmu.edu>
2022-04-27 07:06:14 -0400

    final working

--- src/MapDisplay.java
+++ src/MapDisplay.java
@@ -15,11 +15,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class MapDisplay extends JFrame{
+    DrawnMap map;
 
 
 
 	public MapDisplay(String title,DrawnMap map) {
 
+        this.map=map;
 		super.setTitle(title);
 		super.setSize(1920, 1080);
 		super.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -27,35 +29,57 @@ public class MapDisplay extends JFrame{
 		
 		// setting grid layout with rows, cols, hgap, and vgap
 		super.setLayout(new GridLayout(map.length, map.length, 0, 0));
-
+        
 		init();
+        pack();
 	}
 
-    private void init() {
+
+	
+	private void init() {
 
 		File file = new File("images");
 
 		// getting files name from folder
-		for (String name : file.list()) {
 
-			JLabel label = new JLabel();
+		
 
-			// setting icon
-			label.setIcon(new ImageIcon(
-					new ImageIcon("images/" + name).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
 
-			add(label);
+        for(int k=0;k<50;k++){
+            for(int m=0;m<50;m++){
 
-		}
+                JLabel label = new JLabel();
+                if(!map.map[m][k].type.equals("")){
+                    
+                    String tileType=map.map[m][k].type;
+                    label.setIcon(new ImageIcon(
+            		new ImageIcon("images/" + tileType+".png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+                    add(label);
+                }
+                else{
+                    label.setIcon(new ImageIcon(
+            		new ImageIcon("images/blank.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+                    add(label);
+                    
+                    
+                }
 
-	}
-    
+        
+            }
+        }
+        
+        
+    }
+
 
     public static void main(String args[]){
 
-        String[] ligma={"m","p"};
-        DrawnMap map=new DrawnMap(50, ligma, 1);
-        new MapDisplay("Generated Map",map);
+        String[] test={"m","t"};
+        DrawnMap map=new DrawnMap(50, test, 3);
+        map.populateTypes();
+        map.printMap();
+        MapDisplay display=new MapDisplay("Generated Map",map);
+        
 
     }
 
